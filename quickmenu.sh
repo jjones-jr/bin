e683c484447fc8f9553abe815f092d5bb04a8781
James Jones, Jr <jms_jns@yahoo.com>
2016-09-21 19:56:47 +0900

    gnupg setup check

---
+++ quickmenu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# scriptname:
+# purpose:
+# creation date:
+# (c) James Jones
+
+set -x
+
+sudo
+apt-get autoremove
+apt-cache search
+apt-get update
+apt-get upgrade
+apt-get remove
+apt-get install (local file)
+Re-install App (remove, install)
+App info
+
